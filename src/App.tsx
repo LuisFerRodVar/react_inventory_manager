f89d72c527c2977b517873d81b7b1472e2b25543
LuisFerRodVar <luisfrv1@estudiantec.cr>
2025-05-27 19:38:28 -0600

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Recovery from './pages/Recovery'
+
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/recovery" element={<Recovery />} />
+    </Routes>
+  )
+}
+
+export default App
